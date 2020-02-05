3c4d4e21a7e90a9a434bd5a7ed5ca1592a186b77
Julian-Dac <s3748402@student.rmit.edu.au>
2020-02-06 01:42:54 +1100

    implemented session timeout after 10s possible implementation of session timeout alert box

--- a3-s3725902-s3748402/Startup.cs
+++ a3-s3725902-s3748402/Startup.cs
@@ -1,5 +1,8 @@
+using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -18,6 +21,7 @@ namespace NwbaSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
             services.AddDbContext<NwbaContext>(options =>
             {
                 options.UseSqlServer(Configuration.GetConnectionString(nameof(NwbaContext)));
@@ -30,18 +34,33 @@ namespace NwbaSystem
             services.AddSession(options =>
             {
                 // Make the session cookie essential.
-                options.Cookie.IsEssential = true;
+                options.Cookie.IsEssential = true; // Make the session cookie essential
+                options.Cookie.HttpOnly = true;
+                options.IdleTimeout = TimeSpan.FromSeconds(10); // session will expire after 30 seconds and prompt another login
             });
 
             services.AddControllersWithViews();
             // services.AddHostedService<TimedHostedService>();
             services.AddHostedService<ConsumeScopedServiceHostedService>();
             services.AddScoped<IScopedProcessingService, ScopedProcessingService>();
+            //middleware set the default authentication scheme for the app
+            
+            services.AddAuthentication("CookieAuth")
+                .AddCookie("CookieAuth",config =>
+                {
+                    config.Cookie.Name = "User.Cookie";
+                    config.SlidingExpiration = false;
+                    config.ExpireTimeSpan = TimeSpan.FromSeconds(10);
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            var cookiePolicyOptions = new CookiePolicyOptions
+            {
+                MinimumSameSitePolicy = SameSiteMode.Strict,
+            };
            
             if (env.IsDevelopment())
                 app.UseDeveloperExceptionPage();
@@ -57,7 +76,8 @@ namespace NwbaSystem
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
-            app.UseAuthorization();
+            app.UseAuthentication(); //set the HttpContext.User property and run Authorization Middleware for requests
+            app.UseAuthorization(); //set the HttpContext.User property and run Authorization Middleware for requests
 
             app.UseEndpoints(endpoints =>
             {
@@ -66,7 +86,8 @@ namespace NwbaSystem
 
             app.UseStatusCodePages(
     "text/html", "<html><h1>are you lost son?</h1></html>");
-
         }
+        
+        
     }
 }
